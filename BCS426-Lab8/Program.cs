bdc473f7199a828c7c82234db8bb8d1d744cba7d
alexander-harmaty <92049896+alexander-harmaty@users.noreply.github.com>
2022-04-25 15:55:18 -0400

    comments

--- BCS426-Lab8/Program.cs
+++ BCS426-Lab8/Program.cs
@@ -10,8 +10,20 @@ namespace BCS426_Lab8
     {
         static void Main(string[] args)
         {
+            Registrar registrar = new Registrar();
 
-
+            /**create a new student every N milliseconds 
+             *      where N is a random number between (100-3000). 
+             * You can use Random class to create a random number 
+             *      and Thread.Sleep method to pause your main program 
+             *      in between a new student creations
+             * Create at least 3 new students with appropriate data of your choice 
+             *      (name, date of birth, status, …). 
+             * Note that the registered feature of each student is initially false. 
+             * After creation, each student will fire the newStudentArrived method to be handled by the registrar.
+             * Make sure to add appropriate outputs to your main program that show the user 
+             *      the order of events and how the events are handled.
+            **/
         }
     }
 }
