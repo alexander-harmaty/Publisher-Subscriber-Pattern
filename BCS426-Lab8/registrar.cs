b7154cfe8c4ec697ca03d0d7ae042f21e2f9ba19
alexander-harmaty <92049896+alexander-harmaty@users.noreply.github.com>
2022-04-25 00:16:31 -0400

    Add project files.

---
+++ BCS426-Lab8/registrar.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BCS426_Lab8
+{
+    public class Registrar
+    {
+        Student[] studentList;
+
+    }
+}
---
+++ BCS426-Lab8/student.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BCS426_Lab8
+{
+    class Student
+    {
+        protected string name;
+        protected DateTime DOB;
+        protected string major;
+        protected bool registered;
+        protected enum Status { FRESHMEN, SOPHOMORE, JUNIOR, SENIOR};
+
+        public Student()
+        {
+            name = "NO_NAME";
+            DOB = DateTime.Now;
+            major = "NO_NAME";
+            registered = false;
+        }
+        public Student(string n, DateTime dob, string nmajor, bool nregistered)
+        {
+            name = n;
+            DOB = dob;
+            major = nmajor;
+            registered = nregistered;
+        }
+
+        //public void newStudentArrived()
+    }
+}
